be77cd135bd7695d162b95bf5709cdba400e14b3
Vaibhav Bajaj <97230062+v-aibhavbajaj2520@users.noreply.github.com>
2025-01-16 17:03:39 +0530

    Landing Page
    Updated Nav bar, added names of images in WWD section, effects in contact us page
    + About Us section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,8 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './meetTheTeam/.{html,js}', // Update this path to match your project structure
-    './meetTheTeam/index.html', // Include the main HTML file if needed
-    './meetTheTeam/globals.css',
-    './landing-page.html'
-  ],
+  content: ["./src/meetTheTeam/**/*.html"],
   theme: {
-    extend: {
-      colors: {
-        scrollbarThumb: '#4285f4',
-        scrollbarThumbHover: '#357ae8',
-        scrollbarTrack: '#f1f1f1',
-      },
-      height:{
-        '128':'32rem',
-      },
-      fontFamily: {
-        'Parisienne': ['Parisienne', 'sans-serif'],
-      },
-      animation: {
-        'rotate-360': 'rotate360 1s linear infinite',
-      },
-      keyframes: {
-        rotate360: {
-          '0%': { transform: 'rotate(0deg)' },
-          '100%': { transform: 'rotate(360deg)' },
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [
-    require('tailwind-scrollbar'),
-  ],
-};
-
+  plugins: [],
+}
